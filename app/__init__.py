83e1cb23b8fb881944b8c9df434763cf62076e1c
JMOS_MBUGUA <johnmbugua849@gmail.com>
2019-10-15 10:38:41 +0300

    Create a blueprint function with configuration details and registration of the main blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,26 @@
 from flask import Flask
-from .config import DevConfig
+from config import config_options
+
+  
 
 # Initializing application
-app = Flask(__name__)
+def create_app(config_name):
+    app = Flask(__name__ )
 
-#setting up configuration
-app.config.from_object(DevConfig)
+    #setting up configuration
+    app.config.from_object(config_options[config_name])
+   
+    
+    #registering a blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    
+    #setting config
+    from .requests import configure_request
+    configure_request(app)
+    
+    
+    #will return the app
+    return app
 
-from app import views
+
