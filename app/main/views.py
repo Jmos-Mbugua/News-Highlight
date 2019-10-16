f63938fdfc8406fd672c7504fba8a4360f1fedad
JMOS_MBUGUA <johnmbugua849@gmail.com>
2019-10-16 10:16:06 +0300

    Adds more categories to the view functions

--- app/main/views.py
+++ app/main/views.py
@@ -13,14 +13,30 @@ def index():
     '''
     
     #getting business news sources
-    business_sources = get_sources('business')   
+    business_sources = get_sources('business')
+    general_category = get_sources('general')  
+    entertainment_category = get_sources('entertainment')
+    sports_category = get_sources('sports')
+    technology_category = get_sources('technology')
+    science_category = get_sources('science')
+    health_category =get_sources('health') 
     title = 'Get the latest News  Hightlight '
-    return render_template('index.html', title = title, business = business_sources)
+    return render_template('index.html', title = title, business = business_sources, general = general_category, entertainment = entertainment_category, sports = sports_category, science = science_category, technology = technology_category, health = health_category)
 
 
-@main.route('/headlines/<description>')
-def headlines(description):  
-    
-    #getting top headlines articles
-    headlines_articles = get_headlines(description)
-    return render_template('headlines.html', headlines_articles = headlines_articles)
+
+
+@main.route('/headlines/<id>')
+def articles(id):
+    '''
+    Function that returns articles based on their sources
+    '''
+    news_source = get_headlines(id)
+    title = f'{id} | All Articles'
+
+    return render_template('headlines.html', title = title, name = id, news = news_source)
+
+
+
+
+
